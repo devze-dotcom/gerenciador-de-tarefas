212eea721a31241e067549d3c515353377108e67
devze-dotcom <joseferreiradearaujojunior@gmail.com>
2026-06-16 23:45:33 -0300

    Atualizando as configuracoes do projeto e adicionando a entidade user

--- gerenciador-de-tarefas/src/main/java/com/brasilsoftware/gerenciador_de_tarefas/controller/HelloWorldController.java
+++ gerenciador-de-tarefas/src/main/java/com/brasilsoftware/gerenciador_de_tarefas/controller/HelloWorldController.java
@@ -1,6 +1,10 @@
 package com.brasilsoftware.gerenciador_de_tarefas.controller;
 
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/hello")
 public class HelloWorldController {
 
-    @GetMapping
+   /* @GetMapping
     public String helloword(){
         return "Hello World!";
+    }*/
+
+    @PostMapping
+    public ResponseEntity<String> helloWorld(){
+        return new ResponseEntity<>("Hello World", HttpStatus.CREATED);
     }
 }
---
+++ gerenciador-de-tarefas/src/main/java/com/brasilsoftware/gerenciador_de_tarefas/database/model/User.java
@@ -0,0 +1,45 @@
+package com.brasilsoftware.gerenciador_de_tarefas.database.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.scheduling.config.Task;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 155)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Task> tasks;
+
+    @PrePersist
+    public void prePersist(){
+        this.createdAt = LocalDateTime.now();
+    }
+
+
+}
